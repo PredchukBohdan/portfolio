b9920f765d904b52b5876ed17e12cae6bd3ed9bd
Miosha Tala <miosha.tala@gmail.com>
2025-03-08 15:29:35 +0200

    coding reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 async function getReviews() {
   return await axios.get('https://portfolio-js.b.goit.study/api/reviews');
 }
 function reviewTemplate({ author, avatar_url, review }) {
-  return `<li>
+  return `<li class="swiper-slide">
             <img src="${avatar_url}" alt="${author}'s photo" />
             <h3>${author}</h3>
             <p>${review}</p>
@@ -14,8 +16,33 @@ function reviewsTemplate(arr) {
   return arr.map(reviewTemplate).join('');
 }
 
-const reviewsUl = document.querySelector('.reviews-list');
+const reviewsSwiper = document.querySelector('.reviews-swiper');
+const reviewsUl = document.querySelector('#reviews-list');
 try {
   const response = await getReviews();
   reviewsUl.innerHTML = reviewsTemplate(response.data);
+  const reviewsSwiperConfig = {
+    navigation: {
+      nextEl: '.reviews-next-card',
+      prevEl: '.reviews-previous-card',
+    },
+    slidesPerView: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    mousewheel: true,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+  };
+  const reviewsSwiperInstance = new Swiper(reviewsSwiper, reviewsSwiperConfig);
 } catch {}
