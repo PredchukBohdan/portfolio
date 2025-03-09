a22d12f51824cd777302034a1204096913b4c5c7
Artem-Oganezov <artem_oga@i.ua>
2025-03-09 21:55:41 +0200

    add new file section-faq

---
+++ src/js/faq.js
@@ -0,0 +1,9 @@
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+const option = { 
+    showMultiple: true,
+};
+
+new Accordion('.accordion-wrapper' , option);
+
